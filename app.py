9290410c6fe39d8439209665609c9484bec40a35
hyper <fernandamatos.design@gmail.com>
2026-05-12 19:20:10 -0300

    feat(api): adicionar parâmetros de busca para filtros de cursos
    - Implementada captura de query parameters na rota '/api/cursos'.
    - Adicionados filtros por modalidade, nível e unidade via URL.
    - Integrada a rota com o método de busca genérica do CursoService.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # 'app.py' é o arquivo principal da aplicação Flask. Ele é responsável por criar o servidor web, definir as rotas e integrar o serviço de cursos.
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from scraper.curso_service import CursoService
 
 # Criação da aplicação Flask
 app = Flask(__name__)
+app.json.sort_keys = False
 
 # Instanciação do serviço de cursos
 service = CursoService()
@@ -16,15 +17,20 @@ def home():
 # Rota para obter a lista de cursos em formato JSON.
 @app.route("/api/cursos")
 def cursos():
+    modalidade = request.args.get("modalidade")
+    nivel = request.args.get("nivel")
+    unidade = request.args.get("localizacao")
+
     cursos = service.listar_cursos()
 
+    if modalidade:
+        cursos = service.buscar_por_modalidade(modalidade)
+    if nivel:
+        cursos = service.buscar_por_nivel(nivel)
+    if unidade:
+        cursos = service.buscar_por_unidade(unidade)
+
     return jsonify(cursos)
-
-@app.route("/api/atualizar-cursos")
-def cursos_atualizados():
-    service.atualizar_cursos()
-
-    return jsonify({"message": "Cursos atualizados com sucesso!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
